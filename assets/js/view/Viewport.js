4e53d1c63271d42ad561709cfd5d549c446b8b45
Aaron Ding <web.aaronding@gmail.com>
2015-12-05 22:43:22 -0500

    step 4 main layout

--- assets/js/view/Viewport.js
+++ assets/js/view/Viewport.js
@@ -1,4 +1,50 @@
 Ext.define('Acct.view.Viewport', {
-    extend: 'Ext.container.Viewport',
-    html: 'Hello world!'
+    extend: "Ext.container.Viewport",
+
+    requires: [
+        'Ext.layout.container.Border',
+        'Ext.layout.container.Accordion',
+        'Ext.tab.Panel'
+    ],
+
+    layout: 'border',
+
+    initComponent: function(){
+        var me = this;
+
+        me.items = [{
+            region: 'north',
+            height: 30,
+            html: 'This is a header'
+        }, {
+            region: 'west',
+            title: 'Home',
+            split: true,
+            collapsible: true,
+            width: 300,
+            layout:'accordion',
+            items: [{
+                xtype: 'treepanel',
+                title: 'Main',
+                rootVisible: false,
+                store: {
+                    fields: [{
+                        name: 'text',
+                        mapping: 'name'
+                    }],
+                    root: {children:[{name:'Reports',mtype:'Territory',children:[{name:'Daily report',leaf:true},{name:'Weekly report',leaf:true},{name:'Yearly report',leaf:true}]},{name:'Activities',mtype:'Territory',children:[{name:'USA',mtype:'Country',children:[{name:'Redwood City',mtype:'City',leaf:true}]}]}]}
+                }
+            }, {
+                xtype: 'panel',
+                title: 'User Management',
+                html: 'user'
+            }]
+        }, {
+            region: 'center',
+            xtype: 'tabpanel',
+            html: 'this is a tab'
+        }];
+
+        me.callParent(arguments);
+    }
 });
